92f9846fc8961ab387736135cd949c0f5b48d966
lee-jae-o <dlwodh8746@naver.com>
2024-12-31 17:11:58 +0900

    Mfc commit
    Mfc commit!!

---
+++ MFCImage/MFCImageDlg.h
@@ -0,0 +1,55 @@
+﻿
+// MFCImageDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMFCImageDlg 대화 상자
+class CMFCImageDlg : public CDialogEx
+{
+private:
+	CImage m_image;
+	BOOL validImagePos(int x, int y);
+
+
+	int m_centerX; 
+	int m_centerY;
+
+
+
+// 생성입니다.
+public:
+	CMFCImageDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCIMAGE_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedBtnImage();
+	afx_msg void OnBnClickedBtnSave();
+	afx_msg void OnBnClickedBtnLoad();
+	void UpdateDisplay();
+	void MoveRect();
+	void drawCircle(unsigned char* fm, int i, int j, int nRadius, int nGray);
+	bool isInCircle(int i, int j, int nCenterX, int nCenterY, int nRadius);
+	afx_msg void OnBnClickedBtnAction();
+	afx_msg void OnBnClickedBtnOpen();
+
+
+};
